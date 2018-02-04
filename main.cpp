b8da42e98b37ed348f4fdfd88a0778e76ea6d403
olinekokraglinek <olinekokraglinek@users.noreply.github.com>
2018-02-04 11:58:50 +0100

    commit 3

--- main.cpp
+++ main.cpp
@@ -7,5 +7,17 @@ int main()
     cout << "Hello world!" << endl;
     cout << "************" << endl;
     getchar();
+
+    cout << "Podaj swoje imie: ";
+	cin >> imie;
+
+	cout << "Podaj swoja ulubiona liczbe: ";
+	cin >> liczba;
+	cout << endl;
+
+	cout << "No to zaczynamy " << imie << "!"<< endl;
+	cout << "Niech liczba " << liczba << " bedzie Ci szczesliwa!";
+    getchar();
+
     return 0;
 }
